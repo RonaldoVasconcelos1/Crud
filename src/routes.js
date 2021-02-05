d0a3a92d67afd8a1c5fe886c0c4741da13790c14
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-02-05 16:58:42 -0300

    add login router

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const router = require('express').Router()
 const UserController = require('./Controllers/UserController')
+const Authentication = require('./middlewares/Authentication')
+
+router.post('/login', Authentication.auth)
 
 router.get('/user', UserController.get)
 router.post('/user', UserController.insert)
 router.get('/user/:id', UserController.findById)
+router.get('/user/:email', UserController.findByEmail)
 router.put('/user/:id', UserController.update)
 router.delete('/user/:id', UserController.delete)
 
